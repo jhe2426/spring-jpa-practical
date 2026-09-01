ca2924ae0aad8b5fc7339363446b99d34a1d7482
jhe2426 <jhe2426@naver.com>
2026-09-01 18:06:10 +0900

    스프링 부트와 JPA 활용2 - 회원 등록 API

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.value.Address;
 import lombok.Getter;
 import lombok.Setter;
@@ -229,6 +230,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+//    @NotEmpty
     private String name;
 
     @Embedded
